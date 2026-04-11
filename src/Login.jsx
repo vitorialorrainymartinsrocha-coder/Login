9658ac190fd38fde0fdfb30b941063e051061800
Vitoria Lorrainy <seuemail@gmail.com>
2026-04-11 18:23:11 -0300

    primeiro commit

---
+++ src/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    alert(`Usuário: ${username}, Senha: ${password}`);
+  };
+
+  return (
+    <div className="login">
+      <h2>Tela de Login</h2>
+      <input
+        type="text"
+        placeholder="Usuário"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
